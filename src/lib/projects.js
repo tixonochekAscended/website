cf438162527ed94ba2ef965ec6605164651225e7
tixonochekAscended <tglyph.progl@gmail.com>
2026-07-10 01:35:09 +0300

    new layout for projects, better image importing in projects.js

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,20 +1,17 @@
-import img_oko from "$lib/assets/oko-lang.png"
-import img_oko_VSC from "$lib/assets/oko-lang-vsc.png"
-import img_mrip from "$lib/assets/mini-rip.png"
-import img_mrip_VSC from "$lib/assets/mini-rip-vsc.png"
-import img_toster from "$lib/assets/toster.png"
-import img_tglyph from "$lib/assets/tglyph.png"
-import img_luanite from "$lib/assets/luanite.png"
-import img_doable from "$lib/assets/doable.png"
-import img_lastivka from "$lib/assets/lastivka.png"
-import img_reviung from "$lib/assets/reviung.jpg"
+const img = Object.entries(import.meta.glob([
+    '$lib/assets/*.{png,PNG,jpg,JPG}'
+], { eager: true })).reduce((acc, [path, module]) => {
+    const fullFilename = path.split('/').pop(); 
+    acc[fullFilename] = module.default; 
+    return acc;
+}, {});
 
 export const projs = [
     {
         "name": "Reviung41",
-        "description": "🧰 A custom 40% mechanical keyboard that I've soldered and assembled myself. Hand-crafted tool that makes programming an incredibly comfortable and efficient experience.",
+        "description": "🧰 A custom 40% mechanical keyboard that I've soldered and assembled myself. A hand-crafted tool that makes programming as comfortable as never before.",
         "url": "https://github.com/tixonochekAscended/my-reviung41-config",
-        "img": img_reviung
+        "img": img['reviung.jpg']
     },
 
     {
@@ -31,64 +28,64 @@ export const projs = [
 
     {
         "name": "lastivka",
-        "description": "🐦‍⬛ A discord bot for the aforementioned Discord community, featuring moderation, economy and much more.",
+        "description": "🐦‍⬛ A discord bot for the aforementioned Discord community featuring moderation, economy and much more.",
         "url": "https://discord.gg/NSK7YJ2R6j",
-        "img": img_lastivka
+        "img": img['lastivka.png']
     },
 
     {
         "name": "oko-lang",
         "description": "👁️ An interpreted, non-esoteric programming language. \"Language of vision\".",
         "url": "https://github.com/tixonochekAscended/oko-lang",
-        "img": img_oko
+        "img": img['oko-lang.png']
     },
     
     {
         "name": "oko-lang VSCode",
         "description": "🔌 VSCode extension for the oko-lang programming language.",
         "url": "https://github.com/tixonochekAscended/oko-lang-vscode",
-        "img": img_oko_VSC
-    },
-
-    {
-        "name": "Doable-Proj",
-        "description": "✔️ A task manager with Google integration that helps you get things done. My first project built with SvelteKit.",
-        "url": "https://doable-proj.vercel.app/",
-        "img": img_doable
+        "img": img['oko-lang-vsc.png']
     },
     
     {
         "name": "mini-rip",
         "description": "🪨 A minimal, esoteric, interpreted programming language with magical sigils that execute code.",
         "url": "https://github.com/tixonochekAscended/mini-rip",
-        "img": img_mrip
+        "img": img['mini-rip.png']
     },
     
     {
         "name": "mini-rip VSCode",
         "description": "🔌 VSCode extension for the mini-rip programming language. ",
         "url": "https://github.com/tixonochekAscended/mrip-vscode",
-        "img": img_mrip_VSC
+        "img": img['mini-rip-vsc.png']
     },
     
     {
         "name": "Luanite",
         "description": "📦 Luanite — the project builder that compiles your Lua app into a self-contained executable. Currently archived with no plans for further development.",
         "url": "https://github.com/tixonochekAscended/luanite",
-        "img": img_luanite
+        "img": img['luanite.png']
+    },
+
+    {
+        "name": "Doable-Proj",
+        "description": "✔️ A task manager with Google integration that helps you get things done. My first ever project built with SvelteKit.",
+        "url": "https://doable-proj.vercel.app/",
+        "img": img['doable.png']
     },
 
     {
         "name": "TGlyph (T^)",
-        "description": "☕️ T^ is an esoteric interpreted programming language that is based around \"glyphs\" and \"registers\". Heavy inspiration from APL & Assembly.",
+        "description": "☕️ T^ is an esoteric interpreted programming language that is based around \"glyphs\" and \"registers\".",
         "url": "https://github.com/tixonochekAscended/TGlyph",
-        "img": img_tglyph
+        "img": img['tglyph.png']
     },
 
     {
         "name": "TOSTER (T*)",
         "description": "✨️ An interpreted, esoteric programming language. Made to break conventions. Beware, includes magic.",
         "url": "https://github.com/tixonochekAscended/TOSTER",
-        "img": img_toster
+        "img":  img['toster.png']
     }
 ];
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -16,15 +16,36 @@
     <input bind:value={search} class="font-semibold focus:outline-none lg:w-1/2 w-full text-xl border-1 border-tBLUE/20 bg-tBLUE/5 rounded-xl p-3 text-tYELLOW transition ease-in-out duration-300 hover:border-tBLUE/40 hover:bg-tBLUE/10" type="text" placeholder="🔎 Search projects...">
     <p class="italic">Showing {projects.length} projects</p>
 
-    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        {#each projects as proj (proj.name)}
-        <ProjectCard 
-            href={proj.url} 
-            name={proj.name} 
-            description={proj.description} 
-            img={proj.img}
-            class={projects.indexOf(proj) === projects.length - 1 && projects.length % 2 !== 0 ? "lg:col-span-2 lg:justify-self-center lg:max-w-[50%]" : ""}
-        />
-    {/each}
-    </div>
+    {#if projects.length % 2 == 0}
+        <div class="columns-1 lg:columns-2 space-y-4">
+            {#each projects as proj (proj.name)}
+                <ProjectCard 
+                    href={proj.url} 
+                    name={proj.name} 
+                    description={proj.description} 
+                    img={proj.img}
+                />
+            {/each}
+        </div>
+    {:else}
+        <div class="columns-1 lg:columns-2 space-y-4">
+            {#each projects.slice(0, -1) as proj (proj.name)}
+                <ProjectCard 
+                    href={proj.url} 
+                    name={proj.name} 
+                    description={proj.description} 
+                    img={proj.img}
+                />
+            {/each}
+        </div>
+
+        <div class="flex justify-center items-center">
+            <ProjectCard 
+                href={projects.at(-1).url} 
+                name={projects.at(-1).name} 
+                description={projects.at(-1).description} 
+                img={projects.at(-1).img}
+            />
+        </div>
+    {/if}
 </div>
