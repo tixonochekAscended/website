3aa7284f628df0db857727408be4db5ca03112c7
tixonochekAscended <tglyph.progl@gmail.com>
2026-07-19 01:22:09 +0300

    finally fixed it

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -1,36 +1,6 @@
 <script>
   import { config } from "$lib/config";
 </script>
-<!--
-<svelte:head>
-{#if config.winterTheme}
-  <style>
-    html {
-      min-height: 100%;
-      background-color: #040407;
-    }
-    @media (min-width: 1024px) {
-      html {
-        background: linear-gradient(150deg, rgb(5 5 5 / 0.90) 0%, #050509 50%, #040407 100%);
-      }
-    }
-  </style>
-{:else}
-  <style>
-    html {
-      min-height: 100%;
-      background-color: #040407;
-    }
-    @media (min-width: 1024px) {
-      html {
-        background: linear-gradient(150deg, rgb(14 14 24 / 0.95) 15%, #050509 85%, #040407 100%);
-      }
-    }
-  </style>
-{/if}
-</svelte:head>
--->
- 
 
 <!-- svelte-ignore element_invalid_self_closing_tag -->
-<div class="-z-1000 scale-150 fixed top-0 bottom-0 left-0 right-0 bg-gradient-to-br {config.winterTheme ? "from-[#050505]/93 via-[#050509] to-[#040407]" : "from-[#0e0e18]/93 via-[#050509] to-[#040407]"}"/>
+<div class="-z-1000 scale-125 fixed top-0 bottom-0 left-0 right-0 bg-gradient-to-br {config.winterTheme ? "from-[#050505]/88 via-[#050509] to-[#040407]" : "from-[#0e0e18]/90 via-[#050509] to-[#040407]"}"/>
