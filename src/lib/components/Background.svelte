19a73301d14e31a1d5c786eadc477d2d3322d73a
tixonochekAscended <tglyph.progl@gmail.com>
2026-07-18 22:46:06 +0300

    trying to fix mobile bug

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -3,8 +3,4 @@
 </script>
 
 <!-- svelte-ignore element_invalid_self_closing_tag -->
-{#if config.winterTheme}
-<div class="-z-1000 fixed top-0 left-0 w-full h-full bg-gradient-to-br from-[#050505]/93 via-[#050509] to-[#040407]"/>
-{:else}
-<div class="-z-1000 fixed top-0 left-0 w-full h-full bg-gradient-to-br from-[#0e0e18]/93 via-[#050509] to-[#040407]"/>
-{/if}
+<div class="-z-1000 fixed top-0 bottom-0 left-0 right-0 bg-gradient-to-br {config.winterTheme ? "from-[#050505]/93 via-[#050509] to-[#040407]" : "from-[#0e0e18]/93 via-[#050509] to-[#040407]"}"/>
