d056af7f0717e3ff260351ddf286e6cff8fd2bc8
tixonochekAscended <tglyph.progl@gmail.com>
2026-07-19 11:53:58 +0300

    more bg opacity adjustments

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -3,4 +3,4 @@
 </script>
 
 <!-- svelte-ignore element_invalid_self_closing_tag -->
-<div class="-z-1000 scale-125 fixed top-0 bottom-0 left-0 right-0 bg-gradient-to-br {config.winterTheme ? "from-[#050505]/88 via-[#050509] to-[#040407]" : "from-[#0e0e18]/90 via-[#050509] to-[#040407]"}"/>
+<div class="-z-1000 scale-125 fixed top-0 bottom-0 left-0 right-0 bg-gradient-to-br {config.winterTheme ? "from-[#050505]/88 via-[#050509] to-[#040407]" : "from-[#0e0e18]/92 via-[#050509] to-[#040407]"}"/>
